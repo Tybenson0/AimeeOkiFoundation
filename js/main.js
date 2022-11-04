f8051131712e23df6237e584f829fcc921eb0d62
Tybenson0 <105068044+Tybenson0@users.noreply.github.com>
2022-11-03 21:21:17 -0700

    Aimee-oki-contact

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const hamburgerContainer = document.querySelector(".hamburger-container");
+
+hamburger.addEventListener("click", () => {
+	hamburger.classList.toggle("active")
+	navMenu.classList.toggle("active")
+	hamburgerContainer.classList.toggle("active")
+})
+
+
+/*swiper*/
+
+var swiper = new Swiper(".mySwiper", {
+	effect: "cards",
+	grabCursor: true,
+  });
+
+var swiper = new Swiper(".mySwiper2", {
+	effect: "cards",
+	grabCursor: true,
+  });
+var swiper = new Swiper(".mySwiper3", {
+	effect: "cards",
+	grabCursor: true,
+  });
+
+  
+  
