9c93f55e9a227e2c843f48777bd157cf2d24eebf
Tybenson0 <105068044+Tybenson0@users.noreply.github.com>
2023-01-23 11:46:33 -0800

    photo gallery and bug fixes for section titles.

--- js/main.js
+++ js/main.js
@@ -25,27 +25,7 @@ var swiper = new Swiper(".mySwiper3", {
 	grabCursor: true,
   });
 
- let scrollToBottom = document.querySelector("#scroll-to-bottom")
- let pageBottom = document.querySelector("#page-bottom")
- let scrollToBottom2 = document.querySelector("#second-to-bottom")
- let pageBottom2 = document.querySelector("#second-page-bottom")
- let scrollToBottom3 = document.querySelector("#third-to-bottom")
- let pageBottom3 = document.querySelector("#third-page-bottom")
- let scrollToBottom4 = document.querySelector("#fourth-to-bottom")
- let pageBottom4 = document.querySelector("#fourth-page-bottom")
- 
-scrollToBottom.addEventListener("click", function() {
-  pageBottom.scrollIntoView()
-})
-scrollToBottom2.addEventListener("click", function() {
-	pageBottom2.scrollIntoView()
-  })
-  scrollToBottom3.addEventListener("click", function() {
-	pageBottom3.scrollIntoView()
-  })
-  scrollToBottom4.addEventListener("click", function() {
-	pageBottom4.scrollIntoView()
-  })
+
   
   
 
